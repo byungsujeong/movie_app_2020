6d88a65798e87e35b78a7da39c3047ca4819a9a4
Byungsu Jeong <byungsu.jeong88@gmail.com>
2020-09-30 11:01:42 +0900

    Updates

---
+++ precache-manifest.a46a43068206095f88c2aa658ed25362.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb3f4bda793d50242ee2c559fe0df9ee",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "9b8d39477cc247ac1070",
+    "url": "/movie_app_2020/static/css/main.47afaf7d.chunk.css"
+  },
+  {
+    "revision": "a7d22b2c00d8d326c863",
+    "url": "/movie_app_2020/static/js/2.300fc6d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.300fc6d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b8d39477cc247ac1070",
+    "url": "/movie_app_2020/static/js/main.b8856fd7.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
