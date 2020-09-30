b90dbaf857ca6382af1fd107756effedd04f546d
Byungsu Jeong <byungsu.jeong88@gmail.com>
2020-10-01 04:23:05 +0900

    done

---
+++ src/routes/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./About.css";
+
+const About = (props) => {
+    return (
+        <div className="about__container">
+            <span>
+                About Page
+            </span>
+            <span>about...</span>
+        </div>
+    )
+};
+
+export default About;
